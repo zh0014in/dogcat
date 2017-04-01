77c1cce0fa41fb07023684f896ffc075464b2a21
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-04-01 11:07:44 +0800

    simple plot

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from collections import Counter
 # import plotly.plotly as py
 
 train = pd.read_csv('train.csv',sep=",")
@@ -8,11 +9,38 @@ test = pd.read_csv('test.csv',sep=",")
 # full = np.vstack([train, test])
 
 data_train = np.array(train)
-print data_train[:, 3].T
-plt.hist(data_train[:, 3].T)
-plt.title("Outcome Type Histogram")
-plt.xlabel("Value")
-plt.ylabel("Frequency")
+outcometype_count = Counter(data_train[:, 3])
+outcomesubtype_count = Counter(data_train[:, 4])
+
+def plot_bar_from_counter(counter, ax=None):
+    """"
+    This function creates a bar plot from a counter.
+
+    :param counter: This is a counter object, a dictionary with the item as the key
+     and the frequency as the value
+    :param ax: an axis of matplotlib
+    :return: the axis wit the object in it
+    """
+
+    if ax is None:
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+
+    frequencies = counter.values()
+    names = counter.keys()
+
+    x_coordinates = np.arange(len(counter))
+    ax.bar(x_coordinates, frequencies, align='center')
+
+    ax.xaxis.set_major_locator(plt.FixedLocator(x_coordinates))
+    ax.xaxis.set_major_formatter(plt.FixedFormatter(names))
+
+    return ax
+
+
+plot_bar_from_counter(outcometype_count)
+plt.show()
+plot_bar_from_counter(outcomesubtype_count)
 plt.show()
 # fig = plt.gcf()
 #
