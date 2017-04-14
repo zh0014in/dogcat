07c25c55e0818a9c6e342d192cab1b46a361cc28
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-04-14 11:10:28 +0800

    learning curve

---
+++ learningCurve.py
@@ -0,0 +1,29 @@
+from sklearn.model_selection import learning_curve
+from sklearn.svm import SVC
+import numpy as np
+import pandas as pd
+
+train = pd.read_csv('train_out.csv',sep=",")
+data_train = np.array(train)
+X = data_train[:, 10:]
+y = data_train[:, 3]
+
+test = pd.read_csv('test_out.csv', sep=",")
+data_test = np.array(test)
+X_test = data_test[:, 8:]
+i_test = data_test[:, 0]
+
+c = 300.0
+g = 0.001
+k = 'rbf'
+clf = SVC(C=c, gamma=g, kernel=k,probability = True)
+
+train_sizes, train_scores, valid_scores = learning_curve(
+    clf, X, y, train_sizes=[2000, 4000, 6000], cv=5)
+print train_sizes
+
+print train_scores
+
+
+
+print valid_scores
