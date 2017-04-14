8f68107e87e94e8155ad189c85ca50abdf6ad830
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-04-14 13:07:03 +0800

    data prepare update

--- validationCurve.py
+++ validationCurve.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sklearn.model_selection import validation_curve
-from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 import numpy as np
@@ -8,8 +7,8 @@ import pandas as pd
 
 train = pd.read_csv('train_out.csv',sep=",")
 data_train = np.array(train)
-X = data_train[:, 10:]
-y = data_train[:, 3]
+X = data_train[:10000, 10:]
+y = data_train[:10000, 3]
 
 test = pd.read_csv('test_out.csv', sep=",")
 data_test = np.array(test)
@@ -21,7 +20,7 @@ g = 0.001
 k = 'rbf'
 clf = SVC(C=c, gamma=g, kernel=k,probability = True,cache_size=4000)
 train_scores, valid_scores = validation_curve(clf, X, y, "gamma",
-                                              np.logspace(-7, 3, 3))
+                                              np.logspace(-7, 0, 5))
 print train_scores
 
 print valid_scores
